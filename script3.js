a4def4905342d61b0745583400a273367931df3d
CartridgNSTU <97868606+CartridgNSTU@users.noreply.github.com>
2022-02-21 11:06:28 +0700

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,65 @@
+let students = [
+{id: 1, name: 'Андрей', surname: 'Артамонов'},
+{id: 2, name: 'Дарья', surname: 'Архипова'},
+{id: 3, name: 'Николай', surname: 'Баркалов'},
+{id: 4, name: 'Георгий', surname: 'Бочкарёв'},
+{id: 5, name: 'Матвей', surname: 'Гаврилов'}
+]
+
+let id_current = 0
+
+function load_all () {
+	let table = document.getElementById('tbl_all')
+	for (let i = 0; i< students.length; i++) {
+		let id = students[i].id
+		let name = students[i].name
+		let surname = students[i].surname
+
+		let tr = document.createElement('tr')
+		let td1 = document.createElement('td1')
+		let td2 = document.createElement('td2')
+		let t3 = document.createElement('td3')
+
+		td1.textContent = id
+		td2.textContent = name
+		td3.textContent = surname
+
+
+
+		tr.appendChild(td1)
+		tr.appendChild(td2)
+		tr.appendChild(td3)
+
+		table.appendChild(tr)
+	}
+}
+function load_student(id){
+	let head = document.getElementById('zagolovok')
+	head.textContent = 'Информация о студенте № ' + students[id].id
+
+	let name = document.getElementById('name')
+	document.getElementById('name').textContent = students[id].name
+	name.textContent = ' ' + students[id].name
+
+	let surname = document.getElementById('surname')
+	document.getElementById('surname').textContent = students[id].surname
+	surname.textContent = ' ' + students[id].surname
+}
+
+function next() {
+	id_current++
+	if (id_current > 0) document.getElementById('btn_prev').disabled = false
+	if (id_current === students.length-1) {
+		document.getElementById('btn_next').disabled = true
+	}
+	load_student(id_current)
+}
+
+function prev() {
+	id_current--
+	if (id_current > students.length ) document.getElementById('btn_next').disabled = false
+	if (id_current === 0) {
+		document.getElementById('btn_prev').disabled = true
+	}
+	load_student(id_current)
+}
