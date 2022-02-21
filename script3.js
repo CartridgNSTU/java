b3bc86064386cafdf228855615bd9d9b9ba2474a
CartridgNSTU <97868606+CartridgNSTU@users.noreply.github.com>
2022-02-21 13:57:57 +0700

    update

--- script3.js
+++ script3.js
@@ -2,63 +2,70 @@ let students = []
 let id_current = 0
 
 function load_from_site() {
-	//Создаём объект XMLHttpRequest
 	let xhr = new XMLHttpRequest();
-	//Настраиваем его: GET-запрос по ссылке http://217.71.129.139:4035/students.php
-	xhr.open('Get', 'http://217.71.129.139:4035/students.php');
-	//отсылаем запрос
+	xhr.open ('Get', 'http://217.71.129.139:4035/students.php');
 	xhr.send();
-	//далее код который сработает при ответе сервера
-	xhr.onload = function(){
-		if (xhr.status != 200) { //анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
-			alert('Ошибка ${xhr.status}: ${xhr.statusText}');
+
+	xhr.onload = function () {
+		if (xhr.status != 200) {
+			alert ('Oшибка ${xhr.status}: ${xhr.statusText}');
 		}
-		else {//если всё без ошибок
-			students = JSON.parse(xhr.responseText)['response']
+		else {
+				students = JSON.parse(xhr.responseText)['response']
 		}
 	}
-	xhr.onerror = function() {
-		alert("Запрос не удался");
-	};
+		
+		xhr.onerror = function () {
+			alert("Запрос не удался");
+		};
 }
-function load_all () {
-	let table = document.getElementById('tbl_all')
-	for (let i = 0; i< students.length; i++) {
-		let id = students[i].id
-		let name = students[i].name
-		let surname = students[i].surname
-
-		let tr = document.createElement('tr')
-		let td1 = document.createElement('td1')
-		let td2 = document.createElement('td2')
-		let t3 = document.createElement('td3')
-
-		td1.textContent = id
-		td2.textContent = name
-		td3.textContent = surname
-
-
-
-		tr.appendChild(td1)
-		tr.appendChild(td2)
-		tr.appendChild(td3)
-
-		table.appendChild(tr)
+function load_all() {
+		let table = document.getElementById('tbl_al')
+		for (let i = 0; i < students.length; i++){
+		let id =  students[i].id
+		let name =  students[i].name
+		let surname =  students[i].surname
+	//создадим строку и 3 ячейки
+	 let tr = document.createElement('tr')
+	let td1 = document.createElement('td')
+	let td2 = document.createElement('td')
+	let td3 = document.createElement('td')
+	//запишем данные в ячейки
+	td1.textContent = id
+	td2.textContent = name
+	td3.textContent = surname
+	//вставим ячейки в строку
+	tr.appendChild(td1)
+	tr.appendChild(td2)
+	tr.appendChild(td3)
+	//вставим строку в таблицу
+	table.appendChild(tr)
+}
 	}
-}
+
+let name = document.getElementById('name')
 function load_student(id){
 	let head = document.getElementById('zagolovok')
-	head.textContent = 'Информация о студенте № ' + students[id].id
-
+	head.textContent = "Информация о студенте №" + students[id].id
 	let name = document.getElementById('name')
-	document.getElementById('name').textContent = students[id].name
-	name.textContent = ' ' + students[id].name
-
+	name.textContent =" "+ students[id].name
 	let surname = document.getElementById('surname')
-	document.getElementById('surname').textContent = students[id].surname
-	surname.textContent = ' ' + students[id].surname
-}
+	surname.textContent = " "+students[id].surname
+	let scores = document.getElementById('scores')
+	let sum = 0
+	for (let i = 0; i <students[id].scores.length; i ++) {
+		//console.log(students[id].scores[i]);
+		sum = sum  + students[id].scores[i]
+	}
+		console.log(sum);
+scores.textContent = students[id].scores
+let srscores = document.getElementById('srscores')
+let sr = sum/5;
+srscores.textContent = sr
 
+let logo = document.getElementById('logo').src='http://217.71.129.139:4035/' + students[id].logo
+
+}
 function next() {
 	id_current++
 	if (id_current > 0) document.getElementById('btn_prev').disabled = false
